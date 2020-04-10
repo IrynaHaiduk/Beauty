ad2234492aa01511200f48bd725ad446b35eec2d
Ирина <irinahaiduk@ukr.net>
2020-04-10 19:59:02 +0300

    Делает разметку главной станицы на полной версии

--- gulpfile.js
+++ gulpfile.js
@@ -79,7 +79,7 @@ gulp.task("clean", function () {
 
 gulp.task("copy", function () {
   return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
+    "source/fonts/**/*.{woff,woff2,svg,ttf,eot}",
     "source/img/**",
     "source/js/**",
     "source/*.ico"
